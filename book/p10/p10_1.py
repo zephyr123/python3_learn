0a162267f695eedd689238dcb183d7a0e20630dd
zephyr123 <huys19@qq.com>
2019-05-09 18:06:24 +0800

    p10_1.py

---
+++ book/p10/p10_1.py
@@ -0,0 +1,20 @@
+with open('learning_python.txt') as file_object:
+    contents = file_object.read()
+    print(contents.strip())
+
+print("\n")
+file_name = 'learning_python.txt'
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line.strip())
+
+print("\n")
+file_name = 'learning_python.txt'
+pages = []
+with open(file_name) as file_object:
+    for line in file_object:
+        pages.append(line)
+
+for page in pages:
+    message = "文本的内容是:" + page
+    print(message.rstrip())
