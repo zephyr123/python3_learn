310dacf3226aa14a75864db16effbd7e7050ae7d
zephyr123 <huys19@qq.com>
2019-05-23 19:08:29 +0800

    modify alien_invasion,game_functions,settings,ship

--- book/allien_invasion/alien_invasion.py
+++ book/allien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -13,17 +14,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #创建一艘飞船
     ship = Ship(screen)
-#开始游戏主循环
+    #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ship)
+        ship.update()
         #每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen,ship)
 
 run_game()
--- book/allien_invasion/ship.py
+++ book/allien_invasion/ship.py
@@ -9,10 +9,16 @@ class Ship():
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-
         #将每艘飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        #移动标志
+        self.moving_right = False
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        if self.moving_right:
+            self.rect.centerx +=1
 
     def blitme(self):
         """在指定位置绘制飞船"""
