9f427b736b71ee6aa4f5970ebca3274900a9eead
zephyr123 <huys19@qq.com>
2019-08-18 18:34:53 +0800

    13 zhang

--- book/allien_invasion/alien_invasion.py
+++ book/allien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 import bullet
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 #from alien import Alien
 import game_functions as gf
@@ -14,22 +15,27 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    #创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
     #创建一艘飞船
     ship = Ship(screen,ai_settings)
     #创建一个用于存储子弹的编组
     bullets = Group()
     aliens = Group()
     #创造外星人群
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,ship,aliens)
     #alien = Alien(ai_settings,screen)
 
     #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        # print(len(bullets))
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+            # print(len(bullets))
         #每次循环时都重绘屏幕
         gf.update_screen(ai_settings, screen,ship,aliens,bullets)
 
--- book/allien_invasion/settings.py
+++ book/allien_invasion/settings.py
@@ -6,12 +6,21 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230,230,230)
+        #飞船设置
         self.ship_speed_factor = 1.5 #飞船的设置速度1.5像素
+        self.ship_limit = 3
+
         #子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 3
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction 为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
+
 
 
